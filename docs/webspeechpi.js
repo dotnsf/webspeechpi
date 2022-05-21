d77140dbbb2901e19e4322a3c2648350e9925b7b
K.Kimura <dotnsf@gmail.com>
2022-05-21 09:39:46 +0900

    tagcloud fixed.

--- docs/webspeechpi.js
+++ docs/webspeechpi.js
@@ -76,6 +76,6 @@ function generateTagCloud(){
     word_list.push( { text: tag, weight: tags[tag] } );
   });
 
-  $('#tagcloud').replaceWith( '<div id="tagcloud"></div>' );
+  $('#tagcloud').html( '' );
   $('#tagcloud').jQCloud( word_list, { width: 500, height: 200 } );
 }
